6d245056dc57b6814f9d3e1598c6200fad3c92b1
Dharan Costa <dharancosta@gmail.com>
2022-07-26 21:35:50 -0300

    Deploy configuration

--- src/main/java/com/portifoliodharan/dharanportifolio/DharanportifolioApplication.java
+++ src/main/java/com/portifoliodharan/dharanportifolio/DharanportifolioApplication.java
@@ -1,13 +1,10 @@
 package com.portifoliodharan.dharanportifolio;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class DharanportifolioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DharanportifolioApplication.class, args);
 	}
